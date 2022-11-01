f42483770d95d32596dd702686349c8d182508a2
KaneGreen <737445366KG@Gmail.com>
2022-11-01 12:18:58 +0800

    update document

--- src/lib.rs
+++ src/lib.rs
@@ -11,8 +11,18 @@ pub mod padding_128bit;
 ///// The `aes_with_operation_mode_inplace` mod is similar to `aes_with_operation_mode` but operates inplace.
 //pub mod aes_with_operation_mode_inplace;
 
+/// Miscellaneous functions
 pub mod misc {
-    /// Convert hexadecimal string to 32-bit words (u32) using big endian.
+    /// Convert hexadecimal string to 32-bit words (u32).
+    /// # Examples
+    /// ```
+    /// use aes_frast::misc::hex;
+    /// let word: u32 = hex("0A0B0C0D");
+    /// assert_eq!(word, 0x0D0C0B0A);
+    /// assert_eq!(word, u32::from_le_bytes([0x0A, 0x0B, 0x0C, 0x0D]));
+    /// ```
+    /// # Panics
+    /// This function panics if `s` is not a valid hexadecimal integer in the `u32` range.
     #[inline(always)]
     pub fn hex(s: &str) -> u32 {
         ::std::primitive::u32::from_str_radix(s, 16)
